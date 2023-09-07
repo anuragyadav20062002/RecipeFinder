494f9702cb38209c1eec555853796bb6c4efd8d0
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-09-07 23:31:18 +0530

    add

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const searchBox = document.querySelector('.searchBox');
 const searchBtn = document.querySelector('.searchBtn');
 const recipeContainer = document.querySelector('.recipe-container');
+const recipeDetailsContent = document.querySelector('.recipe-details-container');
+const recipeCloseBtn = document.querySelector('.recipe-close-btn');
 const fetchRecipes = async (query) => {
     recipeContainer.innerHTML = " <h2>Fetching Recipes...</h2>";
 const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
@@ -21,8 +23,18 @@ const button = document.createElement('button');
 button.textContent = "View Recipe" ;
 recipeDiv.appendChild(button);
 recipeContainer.appendChild(recipeDiv);
-});
+button.addEventListener('click' , () =>{
 
+openRecipePopup(meal);
+});
+recipeContainer.appendChild(recipeDiv);
+});
+}
+const openRecipePopup = (meal) => {
+    recipeDetailsContent.textContent=`
+    <h2>${meal.strMeal}</h2>
+    `
+    recipeDetailsContent.parentElement.style.display = "block";
 }
 searchBtn.addEventListener('click' , (e) =>{
     e.preventDefault();
